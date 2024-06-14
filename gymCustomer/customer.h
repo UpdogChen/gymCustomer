4e08c60e44de0d04b7ca6cb03d06b071d20b0d6d
updog <updog86@gmail.com>
2024-06-14 15:25:59 +0800

    test git

--- gymCustomer/customer.h
+++ gymCustomer/customer.h
@@ -1,11 +1,24 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 class customer {
 public:
+	customer(string Name, int Age);
+	customer();
+	void setName(string Name);
+	string getName() const;
+	void setAge(int Age);
+	int getAge() const;
+	virtual void print() const;
 
 private:
-
+	string name;
+	int age;
+	int basicPrice = 1500;
 };
 
 
